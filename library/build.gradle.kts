76ea7e28f35347a48a05fffb83f00b5547c2ca15
Anders Kielsholm <anders@anigif.dk>
2025-02-15 02:25:07 +0100

    Add allWarningsAsErrors flag when building with GitHub Actions as a way of checking that we handle all warnings appropriately

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -43,6 +43,13 @@ kotlin {
             }
         }
     }
+
+    compilerOptions {
+        val allWarningsAsErrorsArgument = "allWarningsAsErrors"
+        if (project.hasProperty(allWarningsAsErrorsArgument)) {
+            allWarningsAsErrors = (project.property(allWarningsAsErrorsArgument) == "true")
+        }
+    }
 }
 
 publishing {
